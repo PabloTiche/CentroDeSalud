1dc8f2aaabeb4c0ddb69517227657450a2bedbc6
PabloTiche <tichepablo@gmail.com>
2023-06-05 11:57:43 -0300

    Controles
    Se agrego la Persona y manejos en los controladores

---
+++ Controlador/ManejoMenu.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CentroDeSalud.Controlador
+{
+    public class ManejoMenu
+    {
+        public int LeerOpcionValida(List<int> opcionesValidas)
+        {
+            int selecc = -1;
+            try
+            {
+                while (!opcionesValidas.Contains(selecc))
+                {
+                    selecc = int.Parse(Console.ReadKey(true).KeyChar.ToString());
+                }
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+
+            return selecc;
+        }
+
+
+    }
+}
--- Modelo/Persona.cs
+++ Modelo/Persona.cs
@@ -6,7 +6,31 @@ using System.Threading.Tasks;
 
 namespace CentroDeSalud.Modelo
 {
-    internal class Persona
+    public class Persona
     {
+        public int numeroDNI {  get; set; }
+
+        public string NombreCompleto { get; set; }
+
+        public string Nacionalidad { get; set; }
+
+        public DateTime FechaNacimiento { get; set; } // int year, int month, int day
+
+        public InfoContacto InfoContacto { get; set; }
+
+        public Direccion Direccion { get; set; }
+
+        public Persona(int dni, string nombreCompleto, string nacionalidad, DateTime fechaNacimiento, InfoContacto infoContacto, Direccion direccion)
+        {
+            this.numeroDNI = dni;
+            this.NombreCompleto = nombreCompleto;
+            this.Nacionalidad = nacionalidad; 
+            this.FechaNacimiento = fechaNacimiento;
+            this.InfoContacto = infoContacto;  
+            this.Direccion = direccion;
+        }
+
+
+
     }
 }
