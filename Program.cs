d7bffac9b622332efcbde8556217684ede3387a3
PabloTiche <tichepablo@gmail.com>
2023-06-06 11:06:51 -0300

    Nuevas Vistas

--- Program.cs
+++ Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+using CentroDeSalud.Vista;
+
+namespace CentroDeSalud
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Menu menu = new Menu();
+            menu.Welcome();
+        }
+    }
+}
--- Vista/Menu.cs
+++ Vista/Menu.cs
@@ -8,5 +8,17 @@ namespace CentroDeSalud.Vista
 {
     internal class Menu
     {
+        public MenuPersona menuPersona = new MenuPersona();
+        public MenuVacuna menuVacuna = new MenuVacuna();
+        public void Welcome()
+        {
+            Console.WriteLine($"--------------------------------\n| Software Centro De Salud |\n--------------------------------\n");
+            Console.WriteLine("Presione una tecla para continuar: "); Console.ReadKey(); Console.Clear();
+        }
+
+        public void MenuPrincipal()
+        {
+            bool salir = false;
+        }
     }
 }
