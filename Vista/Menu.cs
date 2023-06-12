66b07a9114fc97f8980479c4d6f80647f71bb2b5
PabloTiche <tichepablo@gmail.com>
2023-06-11 22:38:07 -0300

    Mejora de funcionamiento y corrección de errores

--- Vista/Menu.cs
+++ Vista/Menu.cs
@@ -13,55 +13,51 @@ namespace CentroDeSalud.Vista
 
         public void MenuWelcome()
         {
-            Console.WriteLine($"--------------------------------\n| Software Centro De Salud |\n--------------------------------\n");
+            Console.WriteLine($"--------------------------------\n|   Software Centro De Salud   |\n--------------------------------\n");
             Console.WriteLine("Presione una tecla para continuar: "); Console.ReadKey(); Console.Clear(); this.MenuPrincipal();
         }
 
         public void MenuPrincipal()
         {
-            ManejoMenu manejoMenu = new();
-            MenuPersona menuPersona = new();
-            MenuVacuna menuVacuna = new();
-            MenuFormulario menuFormulario = new();
+            ManejoMenu manejoMenu = new();  MenuPersona menuPersona = new();
+            MenuVacuna menuVacuna = new(); MenuFormulario menuFormulario = new();
             MenuTurno menuTurno = new();
 
-            bool salir = false;
-            while (!salir)
+            
+            
+            Console.Clear();
+            Console.WriteLine("-- MENU PRINCIPAL --\n\n 0. Salir \n 1. Personas\n 2. Vacunacion \n 3. Formularios \n 4. Turnos");
+
+            int opcion = manejoMenu.LeerOpcionValida(new List<int>() { 0, 1, 2, 3 });
+
+            switch(opcion)
             {
-                Console.Clear();
-                Console.WriteLine("-- MENU PRINCIPAL --\n\n 0. Salir \n 1. Personas\n 2. Vacunacion \n 3. Formularios \n 4. Turnos");
-
-                int opcion = manejoMenu.LeerOpcionValida(new List<int>() { 0, 1, 2, 3 });
-
-                switch(opcion)
-                {
-                    case 0:
-                        {
-                            salir = true;
-                            break;
-                        }
-                    case 1:
-                        {
-                            menuPersona.MenuPrincipalPersona();
-                            break;
-                        }
-                    case 2:
-                        {
-                            menuVacuna.MenuPrincipalVacuna();
-                            break;
-                        }
-                    case 3:
-                        {
-                            menuFormulario.MenuPrincipalFormulario();
-                            break;
-                        }
-                    case 4:
-                        {
-                            menuTurno.MenuPrincipalTurno();
-                            break;
-                        }
-                }
+                case 0:
+                    {    
+                        break;
+                    }
+                case 1:
+                    {
+                        menuPersona.MenuPrincipalPersona();
+                        break;
+                    }
+                case 2:
+                    {
+                        menuVacuna.MenuPrincipalVacuna();
+                        break;
+                    }
+                case 3:
+                    {
+                        menuFormulario.MenuPrincipalFormulario();
+                        break;
+                    }
+                case 4:
+                    {
+                        menuTurno.MenuPrincipalTurno();
+                        break;
+                    }
             }
+            
         }
 
 
--- Vista/MenuPersona.cs
+++ Vista/MenuPersona.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CentroDeSalud.Controlador;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,18 +9,46 @@ namespace CentroDeSalud.Vista
 {
     internal class MenuPersona
     {
-        Menu menuPrincipal = new();
+        Menu menuPrincipal = new(); ManejoMenu manejoMenu = new();
 
         public void MenuPrincipalPersona()
         {
-            Console.WriteLine($"--------------------------------\n| MENU PERSONA |\n--------------------------------\n");
-            Console.WriteLine("\n 0. Volver \n 1. Mostrar \n 2. Buscar \n 3. Cargar \n 4. Modificar");
+            Console.Clear();
+            Console.WriteLine($"------------------------\n|   MENU PERSONA   |\n------------------------\n");
+            Console.WriteLine("\n 0. Volver \n 1. Mostrar Personas \n 2. Buscar Persona \n 3. Cargar Persona \n 4. Modificar Persona");
+
+            int opcion = manejoMenu.LeerOpcionValida(new List<int>() { 0, 1, 2, 3, 4 });
+
+            switch ( opcion )
+            {
+                case 0:
+                    {
+                        menuPrincipal.MenuPrincipal();
+                        break;
+                    }
+                case 1:
+                    {
+                        break;
+                    }
+                case 2:
+                    { 
+                        break;
+                    }
+                case 3:
+                    {
+                        break;
+                    }
+                case 4:
+                    {
+                        break;
+                    }
+            }
         }
 
         public void MenuRegistrarPersona()
         {
             Console.Clear();
-            Console.WriteLine($"--------------------------------\n| MENU REGISTRO PERSONA |\n--------------------------------\n");
+            Console.WriteLine($"---------------------------\n| MENU REGISTRO PERSONA |\n---------------------------\n");
             Console.WriteLine("0. Volver", "1. Registrar Persona");
         }  
         
