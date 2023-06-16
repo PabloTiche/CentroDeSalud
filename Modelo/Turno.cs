8fc11b45445caa2171264e76c5e59b36d67508da
PabloTiche <tichepablo@gmail.com>
2023-06-16 11:53:47 -0300

    Se siguen armando clases

--- Modelo/Turno.cs
+++ Modelo/Turno.cs
@@ -8,5 +8,18 @@ namespace CentroDeSalud.Modelo
 {
     internal class Turno
     {
+        public DateTime FechaTurno { get; set; }
+
+        public TimeOnly HoraTurno { get; set; }
+
+        public Persona Paciente { get; set; }  
+
+        public string MutualPaciente { get; set; }
+
+        public Persona PersonalMedico { get; set; }
+
+        public string? DetallesDeConsulta { get; set; }
+
+
     }
 }
